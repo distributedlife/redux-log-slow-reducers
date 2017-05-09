d4f271c3f18cb1ec212924f0cc9b951ef28a3b1d
Ryan Boucher <ryan.boucher@distributedlife.com>
2017-05-09 16:17:41 +1000

    Use present. Do not run if not in dev mode

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 /* eslint no-console:0 */
+import present from 'present';
 
 export default function logSlowReducers(reducers, thresholdInMs = 8) {
+    if (!__DEV__) {
+        return reducers;
+    }
+
     Object.keys(reducers).forEach((name) => {
         const originalReducer = reducers[name];
+
         reducers[name] = (state, action) => {
-            const start = Date.now();
+            const start = present();
             const result = originalReducer(state, action);
-            const diffInMs = Date.now() - start;
+            const diffInMs = present() - start;
+
             if (diffInMs >= thresholdInMs) {
                 console.warn(`Reducer "${name}" took ${diffInMs}ms for ${action.type}`);
             }
+
             return result;
         };
     });
+
     return reducers;
 }
